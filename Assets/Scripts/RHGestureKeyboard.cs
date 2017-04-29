73087497f3f0a4260c5968baa8123e9b42952c3e
jkamalu <john.kamalu@gmail.com>
2017-04-28 18:04:03 -0700

    Improved implementation by making class usable

--- Assets/Scripts/RHGestureKeyboard.cs
+++ Assets/Scripts/RHGestureKeyboard.cs
@@ -5,8 +5,13 @@ using System.IO;
 public class RabbitHoleGestureKeyboard {
 
 	public static string[] KEYBOARD_LAYOUT = new string[] {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+    private List<string> WORDS = new List<string>();
+    
+    public RabbitHoleGestureKeyboard(string fileName) {
+        populateWords(fileName);       
+    }
 
-	public static bool match(string path, string word) {
+	private bool match(string path, string word) {
 		if (path.Length < word.Length) {
 			return false;
 		}
@@ -25,7 +30,7 @@ public class RabbitHoleGestureKeyboard {
 	}
 
 	// Handles lower case, single character on keyboard, as string
-	public static int getRow(char c) {
+	private int getRow(char c) {
 		string s = c.ToString();
 		for (int i = 0; i < KEYBOARD_LAYOUT.Length; i++) {
 			if (KEYBOARD_LAYOUT[i].Contains(s)) {
@@ -35,7 +40,7 @@ public class RabbitHoleGestureKeyboard {
 		return -1;
 	}
 
-	public static string compressSequence(string sequence) {
+	private string compressSequence(string sequence) {
 		if (sequence.Length == 0) {
 			return string.Empty;
 		}
@@ -51,7 +56,7 @@ public class RabbitHoleGestureKeyboard {
 	}
 
 	// Original code subtracts 3 from return result
-	public static int getMinWordLength(string path) {
+	private int getMinWordLength(string path) {
 		System.Text.StringBuilder sb = new System.Text.StringBuilder();
 		for (int i = 0; i < path.Length; i++) {
 			sb.Append(getRow(path[i]));
@@ -60,20 +65,18 @@ public class RabbitHoleGestureKeyboard {
 		return compression.Length - 3;
 	}
 
-	public static List<string> populateWords(string fileName) {
-		List<string> words = new List<string>();
+	private void populateWords(string fileName) {
 		using (StreamReader sr = new StreamReader(fileName)) {
 			while (!sr.EndOfStream) {
-				words.Add(sr.ReadLine());
+				this.WORDS.Add(sr.ReadLine());
 			}
 		}	
-		return words;
 	}
 
-	public static List<string> getSuggestions(string path, ref List<string> words) {
+	public List<string> getSuggestions(string path) {
 		int minLength = getMinWordLength(path);
 		var filter = 
-			from word in words
+			from word in this.WORDS
 			where
 				(
 				   word[0] == path[0] && 
@@ -90,8 +93,8 @@ public class RabbitHoleGestureKeyboard {
 	}
 
 	static void Main(string[] args) {
-		List<string> words = populateWords("../Resources/dictionary.txt");
-		List<string> suggestions = getSuggestions("wertyuioiuytrtghjklkjhgfd", ref words);
+		RabbitHoleGestureKeyboard RHGK = new RabbitHoleGestureKeyboard("../Resources/dictionary.txt");
+		List<string> suggestions = RHGK.getSuggestions("wertyuioiuytrtghjklkjhgfd");
 		foreach(string sugg in suggestions) {
 			System.Console.WriteLine(sugg);
 		}
